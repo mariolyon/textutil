3db9359a274dbbd0de30578170bac4878bd1e478
Mario Lyon <mario@digileo.com>
2025-09-05 23:17:57 +0200

    change class fields to private

--- main.js
+++ main.js
@@ -1,101 +1,99 @@
-export function assert(condition, message) {
-    if (!condition) {
-        throw message || "Condition unmet.";
-    }
+export function assert (condition, message) {
+  if (!condition) {
+    throw message || 'Condition unmet.'
+  }
 }
 
-
-export function assertType(value, typ) {
-    assert(typeof value === typ, `${value} is not of the expected type ${typ}`)
+export function assertType (value, typ) {
+  assert(typeof typ === 'string', `${typ} is not a string`)
+  assert(typeof value === typ, `${value} is not of the expected type ${typ}`)
 }
 
-export function linesFrom(doc) {
-    assertType(doc, 'string')
-    return doc.trim().split('\n')
+export function linesFrom (doc) {
+  assertType(doc, 'string')
+  return doc.trim().split('\n')
 }
 
 export class Level {
-    static headingRegex = new RegExp("^(#+)");
-    static listRegex = new RegExp("^( *-)");
+  static #headingRegex = new RegExp('^(#+)')
+  static #listRegex = new RegExp('^( *-)')
 
-    constructor(value) {
-        assertType(value, 'string')
-        this.level = Level.levelFor(value)
+  constructor (value) {
+    assertType(value, 'string')
+    this.level = Level.levelFor(value)
+  }
+
+  static levelFor (value) {
+    if (this.#headingRegex.test(value)) {
+      return value.match(this.#headingRegex)[0]
+    } else if (this.#listRegex.test(value)) {
+      return value.match(this.listRegex)[0]
+    } else {
+      return ''
+    }
+  }
+
+  encloses (that) {
+    assert(that instanceof Level)
+    if (this.level.startsWith('#')) {
+      if (that.level.startsWith('#')) {
+        return this.level.length < that.level.length
+      }
+
+      return true
     }
 
-    static levelFor(value) {
-        if (this.headingRegex.test(value)) {
-            return value.match(this.headingRegex)[0]
-        } else if (this.listRegex.test(value)) {
-            return value.match(this.listRegex)[0]
-        } else {
-            return ''
-        }
+    if (this.level.includes('-')) {
+      if (that.level.includes('-')) {
+        return this.level.length < that.level.length
+      }
+
+      if (that.level.includes('#')) {
+        return false
+      }
+
+      return true
     }
 
-    encloses(that) {
-        assert(that instanceof Level)
-        if (this.level.startsWith('#')) {
-            if (that.level.startsWith('#')) {
-                return this.level.length < that.level.length
-            }
-
-            return true
-        }
-
-        if (this.level.includes('-')) {
-            if (that.level.includes('-')) {
-                return this.level.length < that.level.length
-            }
-
-            if (that.level.includes('#')) {
-                return false
-            }
-
-            return true
-        }
-
-        if (that.level.includes('-')) {
-            return that.level.length > 1
-        }
-
-        return false;
+    if (that.level.includes('-')) {
+      return that.level.length > 1
     }
+
+    return false
+  }
 }
 
+export function blocksFrom (doc, tag) {
+  const lines = linesFrom(doc)
+  const blocks = []
+  let blockLines = []
+  let blockLevel = ''
 
-export function blocksFrom(doc, tag) {
-    const lines = linesFrom(doc)
-    const blocks = []
-    let blockLines = []
-    let blockLevel = ''
+  for (const line of lines) {
+    if (blockLines.length > 0) {
+      const lineLevel = new Level(line)
 
-
-    for (const line of lines) {
-        if (blockLines.length > 0) {
-            const lineLevel = new Level(line)
-
-            if (blockLevel.encloses(lineLevel)) {
-                blockLines.push(line)
-            } else {
-                if (blockLines.length > 1) {
-                    blockLines.splice(0, 1)
-                }
-
-                blocks.push(blockLines)
-                blockLines = []
-            }
-
-            continue
+      if (blockLevel.encloses(lineLevel)) {
+        blockLines.push(line)
+      } else {
+        if (blockLines.length > 1) {
+          blockLines.splice(0, 1)
         }
 
-        if (line.includes(`#${tag}`)) {
-            blockLevel = new Level(line)
-            blockLines = []
-            blockLines.push(line)
-            continue
-        }
+        blocks.push(blockLines)
+        blockLines = []
+      }
+
+      continue
     }
 
-    return blocks
+    if (line.includes(`#${tag}`)) {
+      blockLevel = new Level(line)
+      blockLines = []
+      blockLines.push(line)
+      continue
+    }
+  }
+
+  return blocks
 }
